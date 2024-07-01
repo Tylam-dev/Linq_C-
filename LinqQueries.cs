ab36d465cd1bcc3d7b2fce63000731a78f0f8bd5
Tylam.dev <tylam.dev@gmail.com>
2024-07-01 18:22:56 -0500

    Agrega la base del proyecto

---
+++ LinqQueries.cs
@@ -0,0 +1,16 @@
+﻿namespace Linq_C_;
+
+public class LinqQueries
+{
+    private List<Book> librosCollections = new List<Book>();
+   public LinqQueries()
+   {
+        using StreamReader reader = new StreamReader("books.json");
+        string json = reader.ReadToEnd();
+        this.librosCollections = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true })!;
+    }
+   public IEnumerable<Book> TodaLaColeccion()
+   {
+    return librosCollections;
+   }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Linq_C_;
+
 Console.WriteLine("Hello, World!");
+
+LinqQueries queries = new LinqQueries();
+
+ImprimirValores(queries.TodaLaColeccion());
+
+void ImprimirValores(IEnumerable<Book> listdelibros)
+{
+    Console.WriteLine("{0, -70} {1,7} {2,11}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach(var item in listdelibros)
+    {
+        Console.WriteLine("{0, -70} {1,7} {2,11}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
